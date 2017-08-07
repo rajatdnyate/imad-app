97728957316f3b3305a183549b62cd1f0c0dc71e
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-07 15:55:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/Electromania', function(req,res)  { 
-res.sendFile(path.join(__dirname, 'ui', 'Electromania.html'));
+res.sendFile(path.join(__dirname, 'imad-app', 'Electromania.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {

0decb081b41e2540e924066a05048b5de9db4876
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-07 15:32:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/Electromania', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'Electromani.html')); });
 

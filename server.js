a6a8a9352949f1f5c553494bb0f05ef2a621ce6d
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-07 15:29:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/Electromania', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'Electromani.html')); });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

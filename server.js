575ff88845483a898d515a22177cd8ad32bc7e50
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-12 11:01:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-body {
-    font-family: times new roman;
-    background-color: skyblue;
-    margin-top: 20px;
-}
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-.center {
-    text-align:center;
-}
+var app = express();
+app.use(morgan('combined'));
 
-.text-big {
-    font-size: 600%;
-}
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/Electromania', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'Electromani.html')); });
 
-.bold {
-    font-weight: bold;
-}
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
-.img-medium {
-    height: 400px;
-}
-        .container {
-               max-width: 800px;
-               margin:0 auto;
-               color: grey;
-               font-family: brahma;
-           }
-    
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});

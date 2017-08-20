84575b6380614a3a25cf6413937161f0968922c6
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-20 23:23:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = required('pg').Pool;
 
-var config = {
-    user: 'rajat_dnyate',
-    database: 'rajatdnyate',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env. DB_PASSWORD
-    
-};
 var app = express();
 app.use(morgan('combined'));
 
@@ -47,15 +38,4 @@ app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
-var pool = new Pool(config);
-app.get('/test-db', function (res,req) 
-    //make a select request
-    //Get the name from the results
-    pool.query('SELECT * FROM test', function(err,result) {
-        if (err) {
-        res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    })
-    );
+

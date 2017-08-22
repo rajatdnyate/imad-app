c9db73f9d3f0d4b0e534ab58f542aab15aa24bd9
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-22 22:10:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ var express = require('express');
  };
  
  var app = express();
- app.use(morgan('combined'));
- app.use(bodyParser.json());
- app.use(session({
-     secret: 'someRandomSecretValue',
-     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
- }));
+ 
  
  function createTemplate (data) {
      var title = data.title;
@@ -72,38 +67,10 @@ var express = require('express');
  });
  
  
- function hash (input, salt) {
-     // How do we create a hash?
-     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
- }
+
  
  
- app.get('/hash/:input', function(req, res) {
-    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-    res.send(hashedString);
- });
  
- app.post('/create-user', function (req, res) {
-    // username, password
-    // {"username": "tanmai", "password": "password"}
-    // JSON
-    var username = req.body.username;
-    var password = req.body.password;
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           res.send('User successfully created: ' + username);
-       }
-    });
- });
- 
- app.post('/login', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
     
     pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
        if (err) {

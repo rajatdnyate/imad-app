6d2a2b94ae39afbbba2caa8f9313ce5cf9031f13
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-22 08:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,50 +1,136 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
 
-var config = {
-    user: 'rajatdnyate',
-    database: 'rajatdnyate',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-    
-};
 var app = express();
 app.use(morgan('combined'));
-
-var articles = {
-    'article-one': {
-        title: 'Article One! Rajat Dnyate',
-        heading: 'Article One',
-        date: 'August 21, 2017',
-        content: `
-       <p> Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
-            </p>
+var articles = 
+{
+     'article-one'   : {
+    title: 'Article One - Shady Dude',
+    heading: 'Article One',
+    date: "Aug 5. 2017",
+    content: `
             <p>
-                Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
             </p>
+            
             <p>
-                 Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
             </p>`
-    }
+                        },
+     
+     'article-two'   : {
+    title: 'Article Two - Shady Dude',
+    heading: 'Article Two',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+                        },
+     
+     'article-three' : {
+    title: 'Article Three - Shady Dude',
+    heading: 'Article Three',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+         
+                        },
 };
 
+function createTemplate (data) {
+
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" consent="width=device-width, initial-scale=1" />
+        <!--Above Line Is Used For Adjusting Width Of Screen According To Display-->
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+               ${content}
+            </div>
+        </div>
+    </body>
+
+</html>
+                        `;
+    return htmlTemplate;
+
+
+}
+
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+      counter = counter + 1;
+      res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- var counter = 0;
-app.get('/counter', function (req, res) {
-     counter = counter + 1;
-     res.send(counter.toString());
- });
- 
-app.get('/Electromania', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'Electromania.html')); });
+app.get('/:articleName', function (req, res) {
+    //articleName == article-one
+    //articles[articleName] == {} content object for article
+    var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
+});
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -55,7 +141,6 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
@@ -63,35 +148,4 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-var pool = new Pool(config);
-app.get('/test-db', function (req,res) {
-    //make a select request
-    //Get the name from the results
-    pool.query('SELECT * FROM test', function(err,result) {
-        if (err) {
-        res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-    });
-    
-    app.get('/articles/:articleName', function (req ,res) {
-        //articleName == article-one
-        // articles[articleName] =={} content for article one
-    // SELECT  * FROM article WHERE title = 'article-one'
-    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName + "'", function (err, result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
-            } else {
-               var articleDATA = result[0];
-        res.send(createTemplate(articleData));
-            }
-            }
-        });
-        });
         

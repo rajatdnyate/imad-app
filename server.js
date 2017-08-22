850e6858ee4f35b341bdefbd9d55b54d599c11ed
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-22 22:11:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,11 @@ var express = require('express');
        }
     });
  });
+ var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+  app.listen(8080, function () {
+ -  console.log('IMAD course app listening on port 80!');
+ +  console.log(`IMAD course app listening on port ${port}!`);
+  });
  
  app.get('/check-login', function (req, res) {
     if (req.session && req.session.auth && req.session.auth.userId) {

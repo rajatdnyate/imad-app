17e871fb3676f9d7bbc1b4429bbe1b05f5eab286
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-23 10:37:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,53 +1,107 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
 
-var config = {
-    user: 'rajatdnyate',
-    database: 'rajatdnyate',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env. DB_PASSWORD
-    
-};
 var app = express();
 app.use(morgan('combined'));
 
+
 var articles = {
     'article-one': {
-        title: 'Article One! Rajat Dnyate',
-        heading: 'Article One',
-        date: 'August 21, 2017',
-        content: `
-       <p> Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
-            </p>
-            <p>
-                Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
-            </p>
-            <p>
-                 Electromania is a group of 5 engineering students. studying in final year of engineering of electrical and electronics engineering in MIT aurangabad.
-            </p>`
+      title: 'Article One | Tanmai Gopal',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: `
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>`
+    },
+    'article-two': {
+      title: 'Article Two | Tanmai Gopal',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: `
+          <p>
+              This is the content for my second article.
+          </p>`
+    },
+    'article-three': {
+      title: 'Article Three | Tanmai Gopal',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: `
+          <p>
+              This is the content for my third article.
+          </p>`
     }
 };
 
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
- var counter = 0;
+var counter = 0;
 app.get('/counter', function (req, res) {
-     counter = counter + 1;
-     res.send(counter.toString());
- });
- 
-app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'Electromania.html')); });
+  counter = counter + 1;
+  res.send(counter.toString());
+});
 
+app.get('/:articleName', function (req, res) {
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
@@ -55,42 +109,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 8080;
-app.listen(port, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-var pool = new Pool(config);
-app.get('/test-db', function (req,res) {
-    //make a select request
-    //Get the name from the results
-    pool.query('SELECT * FROM test', function(err,result) {
-        if (err) {
-        res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-    });
-    
-    app.get('/articles/:Electromania', function (req ,res) {
-        //articleName == article-one
-        // articles[articleName] =={} content for article one
-    // SELECT  * FROM article WHERE title = 'article-one'
-    pool.query("SELECT * FROM article WHERE title = '"+ req.params.Electromania + "'", function (err, result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
-            } else {
-               var articleDATA = result.rows[0];
-        res.send(createTemplate(articleData));
-            }
-            }
-        });
-        });

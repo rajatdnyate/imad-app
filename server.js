cf68d649d20e63f0e3e53fb6b38ec66bfd6287c5
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-28 21:50:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = required('pg').Pool;
 
-var config = {
-    user: 'rajat_dnyate',
-    database: 'rajatdnyate',
-    host: 'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env. DB_PASSWORD
-    
-};
 var app = express();
 app.use(morgan('combined'));
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/Electromania', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'Electromania.html')); });
-
- var counter = 0;
-app.get('/counter', function (req, res) {
-     counter = counter + 1;
-     res.send(counter.toString());
- });
- 
-app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -49,16 +25,3 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-var pool = new Pool(config);
-app.get('/test-db', function (res,req) 
-    //make a select request
-    //Get the name from the results
-    pool.query('SELECT * FROM test', function(err,result) {
-        if (err) {
-        res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    })
-    );

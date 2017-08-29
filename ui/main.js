b27385a9a338806a9294552f5f3980a57b3ff448
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-29 19:35:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML = 'New value';
 //move the image
 var img = document.getElementById('img');
 img.onclick = function(){
-    img.style.marginleft ='100px';
+    img.style.marginLeft = '200px';
 };

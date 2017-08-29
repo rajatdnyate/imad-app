719dd6a2042f773d4a7845c3750eb1f9beafb950
rajatdnyate <30691434+rajatdnyate@users.noreply.github.com>
2017-08-29 21:10:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,15 @@ app.get('/ui/madi.png', function (req, res) {
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
+var names = [];
+app.get('/submit-name/:name', function(req,res){
+    //Get the name from the request
+    var name = req.params.name;
+    
+    names.push(name);
+    //JSON: javascript object notation
+    res.send(JSON.stringify(names));
+});
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
